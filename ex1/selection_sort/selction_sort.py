9439b92d8f4b5dc04513570dca8c0da6010ff4c3
Yori Verbist <yori.verbist1212@gmail.com>
2022-03-24 15:15:30 +0100

    Visialised the operationts selection sort does

--- ex1/selection_sort/selction_sort.py
+++ ex1/selection_sort/selction_sort.py
@@ -1,20 +1,36 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def sort(elements):
+    operations = 0
     for i in range(len(elements)):
         min = i
         for j in range(i + 1, len(elements)):
+            operations += 1
             if elements[j] < elements[min]:
                 min = j
         elements[i], elements[min] = elements[min], elements[i]
-    return elements
+    return elements, operations
 
 
 def main():
-    numbers = np.random.randint(0, 100, size=50)
-    print(numbers)
-    print(sort(numbers))
+    elements = list()
+    operations_list = list()
+    for i in range(1, 50, 1):
+        numbers = np.random.randint(0, 100, size=i)
+        sorted, operations = sort(numbers)
+        operations_list.append(operations)
+        elements.append(i)
+
+    squares = list(map(lambda x: (x ** 2) / 2, elements))
+    print(squares)
+
+    plt.xlabel("No. of elements")
+    plt.ylabel("No. operations")
+    plt.plot(elements, operations_list, color="b", label="experiment")
+    plt.plot(elements, squares, color="r", label="theoretical")
+    plt.show()
 
 
 if __name__ == "__main__":
